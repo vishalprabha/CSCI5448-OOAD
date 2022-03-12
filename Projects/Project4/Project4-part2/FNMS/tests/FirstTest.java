8a1a21edca8c7bec5eb036ff83a9e2023588128d
AkhilMS7 <srma3452@colorado.edu>
2022-03-11 23:47:36 -0700

    Adding Test Cases

--- Projects/Project4/Project4-part2/FNMS/tests/FirstTest.java
+++ Projects/Project4/Project4-part2/FNMS/tests/FirstTest.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class FirstTest {
     @Test
     public void firstTest() {
-        Assert.assertTrue(true);
+        assertTrue(true);
     }
-
 
     @Test
     public void TestStoreName() {
@@ -26,6 +25,12 @@ public class FirstTest {
         assertEquals(s.announcer.getObserversCount(), 1);
     }
 
+    @Test
+    public void TestRemoveObserver() {
+        Simulation s = new Simulation();
+        s.announcer.removeListener(s.tracker);
+        assertEquals(s.announcer.getObserversCount(), 0);
+    }
 
     @Test
     public void TestLogInstance() {
@@ -42,6 +47,78 @@ public class FirstTest {
         tracker1 = Tracker.getInstance(clerkObjList);
         tracker2 = Tracker.getInstance(clerkObjList);
         assertEquals(tracker1, tracker2);
+    }
+
+    @Test
+    public void TestClerks() {
+        Simulation s = new Simulation();
+        s.initializeStores(1);
+        assertNotEquals(s.specialClerk1, s.specialClerk2);
+    }
+
+    @Test
+    public void TestInit() {
+        MusicStore musicStoreObj = new MusicStore("North");
+        Assert.assertNotNull(musicStoreObj);
+    }
+
+    @Test
+    public void TestClerkSize() {
+        MusicStore musicStoreObj = new MusicStore("");
+        int size = musicStoreObj.getClerkObjList().size();
+        assertTrue("", size <= 6);
+    }
+
+    @Test
+    public void TestTuning0() {
+        ElectronicTune electronicTune = new ElectronicTune();
+        double purchasePrice = OuterUtils.Utils.getRandomPrice(1,50);
+        Players cassettePlayer = new CassettePlayer(OuterUtils.Utils.getRandomName(), purchasePrice, purchasePrice*2, "New", 0, "excellent", false);
+        electronicTune.tune(cassettePlayer, "SouthStore");
+        assertTrue(cassettePlayer.equalized);
+    }
+
+    @Test
+    public void TestTuning1() {
+        HaphazardTune haphazardTune = new HaphazardTune();
+        double purchasePrice = OuterUtils.Utils.getRandomPrice(1,50);
+        Players cassettePlayer = new CassettePlayer(OuterUtils.Utils.getRandomName(), purchasePrice, purchasePrice*2, "New", 0, "excellent", false);
+        assertFalse(cassettePlayer.equalized);
+    }
+
+    @Test
+    public void TestFactory() {
+        NorthSideFactory northSideFactory = new NorthSideFactory();
+        List<Bridge> bridges=  northSideFactory.createBridge();
+        assertEquals(bridges.size(),3);
+    }
+
+    @Test
+    public void TestSize() {
+        int r = OuterUtils.Utils.getRandomInt(10,20);
+        assertTrue("", r < 20);
+    }
+
+    @Test
+    public void TestItemTypes() {
+        MusicStore musicStoreObj = new MusicStore("North");
+        assertEquals(musicStoreObj.getInventoryObj().ItemsList.size(), 63);
+    }
+
+    @Test
+    public void TestMoney() {
+        Simulation s = new Simulation();
+        Clerk clerkObj = new Clerk( 20, "Shaggy", new ManualTune());
+        CashRegister cashRegisterObj = new CashRegister(0, 0);
+        clerkObj.checkRegisterObj.checkBalance(1, cashRegisterObj, s.announcer, clerkObj.name, "storeName");
+        assertTrue("Not equals", cashRegisterObj.money -  1000.0 == 0);
+    }
+
+    @Test
+    public void TestCovers() {
+        SouthSideFactory southSideFactory = new SouthSideFactory();
+        List<Covers> covers=  southSideFactory.createCovers();
+        assertEquals(covers.size(),3);
     }
 
 }
