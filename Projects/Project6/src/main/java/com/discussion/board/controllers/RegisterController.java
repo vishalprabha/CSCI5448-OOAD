054bfa3aed121b93bf986020d4f876aedc5d00e4
vishalprabha <vishal.prabhachandar1@gmail.com>
2022-04-13 20:57:49 -0600

    Project 6 commit

---
+++ Projects/Project6/src/main/java/com/discussion/board/controllers/RegisterController.java
@@ -0,0 +1,65 @@
+package com.discussion.board.controllers;
+
+import com.discussion.board.repositories.UserRepository;
+import com.discussion.board.entities.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Controller
+public class RegisterController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegisterController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @GetMapping("register")
+    public String displayRegister(Model model) {
+        return "register";
+    }
+
+    @PostMapping("register")
+    public View registerUser(@RequestParam("username") String username, @RequestParam("password") String password,
+                             @RequestParam("introduction") String introduction, HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        User user = new User();
+        if (userRepository.getUserByUsername(username) == null) {
+            user.setUsername(username);
+            // I know that it can be blank field, but I did it on purpose to find out about Optionals:
+            if (Objects.equals(introduction, ""))
+                user.setIntroduction(null);
+            else
+                user.setIntroduction(introduction);
+            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
+            user.setCreatedDate(LocalDateTime.now());
+            userRepository.save(user);
+            return new RedirectView(contextPath + "/login");
+        } else
+            return new RedirectView(contextPath + "/register");
+    }
+}
